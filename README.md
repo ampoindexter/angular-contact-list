3bda0ad757a491b91edcad433b5815e0818f2952
Alicia Poindexter <alcampbel@gmail.com>
2016-02-09 14:29:14 -0800

    edit functionality is working

---
+++ README.md
@@ -0,0 +1,5 @@
+# ANGULAR CONTACT LIST
+
+## By Alicia Poindexter
+
+Deployed at: http://ampoindexter.github.io/angular-contact-list/
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict'; 
 
 var app = angular.module('MyApp', ['ngStorage']);
+var i;
 
 app.controller('mainCtrl', function($scope, $localStorage, $sessionStorage) {
   $scope.$storage = $localStorage;
@@ -20,6 +21,8 @@ app.controller('mainCtrl', function($scope, $localStorage, $sessionStorage) {
 
   $scope.contacts = $scope.$storage.contacts;
 
+  $scope.editing = false
+
   $scope.addContact = function() {
     $scope.contacts.push($scope.newContact);
     $scope.newContact = {};
@@ -28,6 +31,23 @@ app.controller('mainCtrl', function($scope, $localStorage, $sessionStorage) {
   $scope.deleteContact = function(contact) {
     var index = $scope.contacts.indexOf(contact);
     $scope.contacts.splice(index, 1);
+  }
+
+  $scope.startEdit = function(contact) {
+    $scope.editing = !$scope.editing;
+    $('#editName').val(contact.name);
+    $('#editEmail').val(contact.email);
+    $('#editPhone').val(contact.phone);
+    i = $scope.contacts.indexOf(contact);
+  }
+
+  $scope.editContact = function() {
+    var editedContact = {};
+    editedContact.name = $('#editName').val();
+    editedContact.email = $('#editEmail').val();
+    editedContact.phone = $('#editPhone').val();
+    $scope.contacts.splice(i, 1, editedContact);
+    $scope.editing = !$scope.editing;
   }
 });
 
