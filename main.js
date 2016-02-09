85179a76e64f2d31c87d81548a4361db771e702f
Alicia Poindexter <alcampbel@gmail.com>
2016-02-09 09:02:19 -0800

    add and delete functionality

---
+++ main.js
@@ -0,0 +1,33 @@
+'use strict'; 
+
+var app = angular.module('MyApp', ['ngStorage']);
+
+app.controller('mainCtrl', function($scope, $localStorage, $sessionStorage) {
+  $scope.$storage = $localStorage;
+  if(!$scope.$storage.contacts) {
+    $scope.$storage = $localStorage.$default({
+      contacts: [
+        { name: 'Sally', email: 'sally@email.com', phone: '555-347-9876' },
+        { name: 'Bill', email: 'bill@email.com', phone: '555-983-4472' },
+        { name: 'John', email: 'john@email.com', phone: '555-393-4921' },
+        { name: 'Lisa', email: 'lisa@email.com', phone: '555-742-3390' }
+      ]
+    });
+  }
+  console.log('hello from mainCtrl!');
+
+  $scope.keys = Object.keys;
+
+  $scope.contacts = $scope.$storage.contacts;
+
+  $scope.addContact = function() {
+    $scope.contacts.push($scope.newContact);
+    $scope.newContact = {};
+  }
+
+  $scope.deleteContact = function(contact) {
+    var index = $scope.contacts.indexOf(contact);
+    $scope.contacts.splice(index, 1);
+  }
+});
+
